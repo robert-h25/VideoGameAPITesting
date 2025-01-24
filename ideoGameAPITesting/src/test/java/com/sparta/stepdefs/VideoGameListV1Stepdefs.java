f60d929c64e891ffc53477c07f4d6c5d35c05f9d
ik1g19 <isaacklugman@gmail.com>
2025-01-24 15:12:16 +0000

    finish videogame list stepdef happy path

--- ideoGameAPITesting/src/test/java/com/sparta/stepdefs/VideoGameListV1Stepdefs.java
+++ ideoGameAPITesting/src/test/java/com/sparta/stepdefs/VideoGameListV1Stepdefs.java
@@ -6,6 +6,9 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 public class VideoGameListV1Stepdefs {
     Response response;
     RequestSpecification request;
@@ -28,11 +31,12 @@ public class VideoGameListV1Stepdefs {
 
     @Then("I should receive a list of video games in a list")
     public void i_should_receive_a_list_of_video_games_in_a_list() {
-        //assertThat();
+        assertThat("Response body should not be null", response.getBody().asString(), is(not(emptyString())));
+        assertThat("Response should contain a list of video games", response.jsonPath().getList("$"), is(not(empty())));
     }
 
     @And("I get 200 status code")
     public void i_get_200_status_code() {
-
+        assertThat("Status code should be 200", response.getStatusCode(), is(200));
     }
 }
