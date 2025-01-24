a750362ab7824e88445d4b5b7aae61ade2cac5dc
tcalabangiu <tcalabangiu@spartaglobal.com>
2025-01-24 14:37:47 +0000

    Create test classing for DELETE and added methods to get RequestSpecification with unique id inside Utils.java

--- ideoGameAPITesting/src/test/java/com/sparta/Utils.java
+++ ideoGameAPITesting/src/test/java/com/sparta/Utils.java
@@ -1,27 +1,48 @@
 package com.sparta;
 
+import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 
 public class Utils {
-    private static final String BASE_URL = "https://videogamedb.uk:443/api";
+    private static final String BASE_URI = "https://videogamedb.uk:443/api";
     private static final String BASE_PATH_TO_VIDEOGAME = "/videogame";
-    private static final String POST_COMMENT_VIDEOGAME_ID = "/videogame/{id}";
+    private static final String BASE_PATH_TO_VIDEOGAME_ID = "/videogame/{id}";
     private static final String BASE_PATH_WITH_V2_VIDEOGAME = "/v2/videogame";
     private static final String BASE_PATH_WITH_V2_VIDEOGAME_ID = "/v2/videogame/{id}";
 
+    private static final String BEARER_TOKEN =
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTczNzcyODUzMCwiZXhwIjoxNzM3NzMyMTMwfQ.oL2RLBDwo-9I0Vqz9LKjIFMa7OSAWu9eoH82MFKQL-4";
+
+    static int id;
 
 
     private static RequestSpecBuilder getBaseSpecBuilder() {
         return new RequestSpecBuilder()
-                .setBaseUri(BASE_URL)
+                .setBaseUri(BASE_URI)
                 .addHeaders(Map.of(
                         "Accept", "application/json"
                 ));
     }
+
+    private static RequestSpecBuilder getBaseSpecBuilderWithPath(String path, Integer id) {
+        return new RequestSpecBuilder()
+                .setBaseUri(path)
+                .addPathParams(Map.of(
+                        "id", id
+                ))
+                .addHeaders(Map.of(
+                        "Accept", "application/json"
+                ));
+    }
+
+
     static RequestSpecification requestSpecification(Integer id,String body) {
         return getBaseSpecBuilder()
 
