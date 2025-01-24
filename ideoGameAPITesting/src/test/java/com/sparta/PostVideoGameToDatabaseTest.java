56e2c17e8f4c28bf49ff2a583c8649b9afd97d95
iahmad-sparta <iahmad@spartaglobal.com>
2025-01-24 15:55:16 +0000

    david-ibrahim | Adding post request specification and tests for adding game to database.

---
+++ ideoGameAPITesting/src/test/java/com/sparta/PostVideoGameToDatabaseTest.java
@@ -0,0 +1,44 @@
+package com.sparta;
+
+import io.restassured.response.Response;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PostVideoGameToDatabaseTest {
+
+    @Test
+    @DisplayName("Post Video Game to Video Game Database Version 1")
+    public void postVideoGameToDatabase_v1() {
+        Response response = Utils.createVideoGame_AndPostToDatabase_v1(0, "Platform", "Luigi's Mansion", "Mature", "2001-11-18", 86);
+        MatcherAssert.assertThat(response.getStatusCode(), Matchers.equalTo(200));
+    }
+
+    @Test
+    @DisplayName("Post Video Game to Video Game Database Version 2")
+    public void postVideoGameToDatabase_v2() {
+        Response response = Utils.createVideoGame_AndPostToDatabase_v2(0, "Platform", "Luigi's Mansion", "Mature", "2001-11-18", 86);
+        MatcherAssert.assertThat(response.getStatusCode(), Matchers.equalTo(200));
+    }
+
+    @Test
+    @DisplayName("Post video game that exists in the video database Version 2")
+    public void postVideoGameThatExistsInDatabase_V1(){
+        Response response = Utils.createVideoGame_AndPostToDatabase_v1(1, "Shooter", "Resident Evil 4", "Universal", "2005-10-01 23:59:59 ",
+                85
+        );
+        MatcherAssert.assertThat(response.getStatusCode(), Matchers.equalTo(400));
+    }
+
+
+    @Test
+    @DisplayName("Post video game that exists in the video database Version 2")
+    public void postVideoGameThatExistsInDatabase_V2(){
+        Response response = Utils.createVideoGame_AndPostToDatabase_v2(2, "Driving", "Gran Turismo 3", "Universal", "2001-03-10 23:59:59",
+                85
+        );
+        MatcherAssert.assertThat(response.getStatusCode(), Matchers.equalTo(400));
+    }
+
+}
--- ideoGameAPITesting/src/test/java/com/sparta/Utils.java
+++ ideoGameAPITesting/src/test/java/com/sparta/Utils.java
@@ -1,5 +1,7 @@
 package com.sparta;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
@@ -8,12 +10,38 @@ import java.util.Map;
 
 public class Utils {
     private static final String BASE_URL = "https://videogamedb.uk:443/api";
-    private static final String BASE_PATH_TO_VIDEOGAME = "/videogame";
-    private static final String POST_COMMENT_VIDEOGAME_ID = "/videogame/{id}";
-    private static final String BASE_PATH_WITH_V2_VIDEOGAME = "/v2/videogame";
+    private static final String BASE_PATH_WITH_V1_VIDEOGAME_ID = "/videogame/{id}";
+    private static final String POST_PATH_WITH_VIDEOGAME = "/videogame";
+    private static final String POST_PATH_WITH_V2_VIDEOGAME = "/v2/videogame";
     private static final String BASE_PATH_WITH_V2_VIDEOGAME_ID = "/v2/videogame/{id}";
+    private static final String BEARER_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTczNzczMDgzMCwiZXhwIjoxNzM3NzM0NDMwfQ.eHVPHfKPnJRAOWt3BG8OC7CYAdgyDCS-wp4gI30CC0I";
 
 
+    static RequestSpecification getVideoGameWithId_V1(Integer id){
+        return new RequestSpecBuilder()
+                .setBaseUri(BASE_URL)
+                .setBasePath(BASE_PATH_WITH_V1_VIDEOGAME_ID)
+                .addHeaders(Map.of(
+                        "Accept", "application/json"
+                ))
+                .addPathParams(Map.of(
+                        "id", id
+                ))
+                .build();
+    }
+
+    static RequestSpecification getVideoGame_WithId_v2(Integer id){
+        return new RequestSpecBuilder()
+                .setBaseUri(BASE_URL)
+                .setBasePath(BASE_PATH_WITH_V2_VIDEOGAME_ID)
+                .addHeaders(Map.of(
+                        "Accept", "application/json"
+                ))
+                .addPathParams(Map.of(
+                        "id", id
+                ))
+                .build();
+    }
 
     private static RequestSpecBuilder getBaseSpecBuilder() {
         return new RequestSpecBuilder()
@@ -38,5 +66,60 @@ public class Utils {
                 .build();
     };
 
+    static RequestSpecification postVideoGame_v1(PostVideoGameRequestBody requestBody) {
+        return new RequestSpecBuilder()
+                //TODO: May need to include bearer token
+                .addHeaders(Map.of(
+                        "Accept", "application/json",
+                        "Content-Type", "application/json",
+                        "Authorization", "Bearer " + BEARER_TOKEN
+                ))
+                .setBaseUri(BASE_URL)
+                .setBasePath(POST_PATH_WITH_VIDEOGAME)
+                .setContentType(ContentType.JSON)
+                .setBody(requestBody)
+                //TODO: Need to add request body json, records...
+                .build();
+    }
 
+    static RequestSpecification postVideoGame_v2(PostVideoGameRequestBody requestBody) {
+        return new RequestSpecBuilder()
+                //TODO: May need to include bearer token
+                .addHeaders(Map.of(
+                        "Accept", "application/json",
+                        "Content-Type", "application/json"
+                ))
+                .setBaseUri(BASE_URL)
+                .setBasePath(POST_PATH_WITH_V2_VIDEOGAME)
+                .setContentType(ContentType.JSON)
+                .setBody(requestBody)
+                //TODO: Need to add request body json, records...
+                .build();
+    }
+
+    static Response createVideoGame_AndPostToDatabase_v1(int id, String category, String name, String rating, String releaseDate, int reviewScore) {
+        Response response =
+                RestAssured
+                        .given(postVideoGame_v1(new PostVideoGameRequestBody(id, category, name, rating, releaseDate, reviewScore)))
+                        .when()
+                        .post()
+                        .then()
+                        .log().all()
+                        .assertThat().statusCode(200)
+                        .extract().response();
+        return response;
+    }
+
+    static Response createVideoGame_AndPostToDatabase_v2(int id, String category, String name, String rating, String releaseDate, int reviewScore) {
+        Response response =
+                RestAssured
+                        .given(postVideoGame_v2(new PostVideoGameRequestBody(id, category, name, rating, releaseDate, reviewScore)))
+                        .when()
+                            .post()
+                        .then()
+                            .log().all()
+                            .assertThat().statusCode(200)
+                            .extract().response();
+        return response;
+    }
 }
